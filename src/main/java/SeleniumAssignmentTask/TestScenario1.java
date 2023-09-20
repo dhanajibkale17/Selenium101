1859a4eae9a02e9f089f98a5e1be2e197906aa7f
Dhanaji Kale <Dhanaji Kale@DK-01C13DG>
2023-09-20 16:41:50 +0530

    second commit..

--- src/main/java/SeleniumAssignmentTask/TestScenario1.java
+++ src/main/java/SeleniumAssignmentTask/TestScenario1.java
@@ -7,9 +7,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TestScenario1 {
+public class TestScenario1 extends TestScenario2 {
 
-	public static void main(String[] args) throws Exception {
+	public void TestAss1() throws Exception {
+
 		//1.Open LambdaTest’s Selenium Playground from
 		WebDriverManager.chromedriver().setup();		
 		// Launching Chrome Browser Instance
@@ -18,45 +19,59 @@ public class TestScenario1 {
 		driver.manage().window().maximize();
 		// Open Application URL 
 		driver.get("https://www.lambdatest.com/selenium-playground");
-		
+
 		//2. Click “Simple Form Demo” under Input Forms.
 		driver.findElement(By.xpath("//a[normalize-space()='Simple Form Demo']")).click();
-		
+
 		//3. Validate that the URL contains “simple-form-demo”.
 		String url=driver.getCurrentUrl();
 		System.out.println("URL: " +url);
 		String urlTitle =driver.getTitle();
 		System.out.println("URL Title: "+ urlTitle);
-		
+
 		if(driver.getCurrentUrl().endsWith("simple-form-demo")) {
 			System.out.println("URL contains Passed ");
 		}else {
 			System.out.println("URL contains Failed ");
 		}
-		
+
 		//4. Create a variable for a string value E.g: “Welcome to LambdaTest”
-		
+
 		String StringValue="Welcome to LambdaTest";
-		
+
 		//5.Use this variable to enter values in the “Enter Message” text box.
 		driver.findElement(By.id("user-message")).sendKeys(StringValue);
-		
+
 		//6. Click “Get Checked Value"
 		driver.findElement(By.id("showInput")).click();	
-		
+
 		Thread.sleep(2000);
-		
+
 		//7. Validate whether the same text message is displayed in the right-hand
 		//    panel under the “Your Message:” section.
 		String str1=driver.findElement(By.id("user-message")).getAttribute("value");
 		System.out.println(str1);
 		String str2=driver.findElement(By.id("message")).getText();	
 		System.out.println(str2);
-		
+
 		if(str1.equals(str2)) {
 			System.out.println("Passed");
 		}else
-			System.out.println("Failed");
+			System.out.println("Failed");	
 		
+		driver.quit();
 	}
+
+	public static void main(String[] args) throws Exception 
+	{
+		//TestScenario1 Test1=new TestScenario1();
+		//Test1.TestAss1();
+		
+		Thread.sleep(2000);
+
+		TestScenario2 Test2=new TestScenario2();
+		Test2.TestAss2();
+
+	}
+
 }
--- src/main/java/SeleniumAssignmentTask/TestScenario2.java
+++ src/main/java/SeleniumAssignmentTask/TestScenario2.java
@@ -9,13 +9,14 @@ import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TestScenario2 {
+public class TestScenario2 extends TestScenario3{
 
-	public static void main(String[] args) throws Exception {	
-		
+
+	public void TestAss2() throws Exception {
+
 		//1. Open the https://www.lambdatest.com/selenium-playground page and
 		//   click “Drag & Drop Sliders” under “Progress Bars & Sliders”.
-		
+
 		WebDriverManager.chromedriver().setup();		
 		// Launching Chrome Browser Instance
 		WebDriver driver = new ChromeDriver();
@@ -23,39 +24,54 @@ public class TestScenario2 {
 		driver.manage().window().maximize();
 		// Open Application URL 
 		driver.get("https://www.lambdatest.com/selenium-playground");
-		
+
 		// click “Drag & Drop Sliders” under “Progress Bars & Sliders”.
 		driver.findElement(By.xpath("//a[normalize-space()='Drag & Drop Sliders']")).click();
-		
+
 		//2. Select the slider “Default value 15” and drag the bar to make it 95 by
 		//   validating whether the range value shows 95.
 		//	Thread.sleep(2000);		
-		
+
 		WebElement slider = driver.findElement(By.xpath("//input[@value='15']"));
-		
+
 		//System.out.println("Location of the slider"+slider.getLocation());
 		//System.out.println("Height & Width of the slider"+slider.getSize());		
 		//Thread.sleep(2000);
-		
+
 		Actions act=new Actions(driver);		
 		act.dragAndDropBy(slider,215, 0).perform();
-		
-		
+
+
 		//System.out.println("Location of the slider"+slider.getLocation());
 		//System.out.println("Height & Width of the slider"+slider.getSize());		
 		//driver.findElement(By.id("rangeSuccess")).getText();
 		//System.out.println(""+driver.findElement(By.id("rangeSuccess")).getText());
-		
-        String expectedValue="95";		
-	    String actualValue=driver.findElement(By.id("rangeSuccess")).getText(); 	    
-	    System.out.println(actualValue);
-	    
-	    try {
-	    	Assert.assertEquals(actualValue, expectedValue, "Both Value are not matching");
+
+		String expectedValue="95";		
+		String actualValue=driver.findElement(By.id("rangeSuccess")).getText(); 	    
+		System.out.println(actualValue);
+
+		try {
+			Assert.assertEquals(actualValue, expectedValue, "Both Value are not matching");
 		} catch (AssertionError a) {
 			//a.printStackTrace();
 		}
-		System.out.println("Slider Range Value Valid Sucess.");			
+		System.out.println("Slider Range Value Valid Sucess.");		
+		
+		driver.quit();
+
+	}
+
+	public static void main(String[] args) throws Exception {	
+		
+		//TestScenario2 Test2=new TestScenario2();
+		//Test2.TestAss2();
+		
+		Thread.sleep(2000);
+		
+		TestScenario3 Test3=new TestScenario3();
+		Test3.TestAss3();
+
 		
 	}
 }
